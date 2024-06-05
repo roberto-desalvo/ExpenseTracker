dc5af069736f7438c5c68306d5625fbfd37071a2
Roberto De Salvo <roberto.desalvo@msc.com>
2024-06-05 18:19:32 +0200

    some refactor

--- RDS.ExpenseTracker.Desktop.WPF/Views/MainView.xaml.cs
+++ RDS.ExpenseTracker.Desktop.WPF/Views/MainView.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using RDS.ExpenseTracker.Business.Helpers.Abstractions;
 using RDS.ExpenseTracker.Desktop.WPF.Controls;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace RDS.ExpenseTracker.Desktop.WPF.Views
 {
