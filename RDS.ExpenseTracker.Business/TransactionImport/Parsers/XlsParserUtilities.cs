ff3d25584798b23e070c83eb6258ce92322e606d
roberto-desalvo <roberto.desalvo@outlook.com>
2024-12-27 17:44:48 +0100

    added consts for xls file import

--- RDS.ExpenseTracker.Business/TransactionImport/Parsers/XlsParserUtilities.cs
+++ RDS.ExpenseTracker.Business/TransactionImport/Parsers/XlsParserUtilities.cs
@@ -8,7 +8,6 @@ namespace RDS.ExpenseTracker.Business.TransactionImport.Parsers
 {
     public static class XlsParserUtilities
     {
-
         public static DateTime ParseDateFromSheetName(string name)
         {
             var index = name.IndexOf('2');
@@ -23,18 +22,18 @@ namespace RDS.ExpenseTracker.Business.TransactionImport.Parsers
 
         public static XlsDataRowModel GetDataRowModel(DataRow dataRow)
         {
-            var transactionOutflow = Utils.DecimalToCubedInt(dataRow[2].ParseToDecimal()) ?? 0;
-            var transactionInflow = Utils.DecimalToCubedInt(dataRow[3].ParseToDecimal()) ?? 0;
+            var transactionOutflow = Utils.DecimalToCubedInt(dataRow[XlsConstants.TransactionOutflowIndex].ParseToDecimal()) ?? 0;
+            var transactionInflow = Utils.DecimalToCubedInt(dataRow[XlsConstants.TransactionInflowIndex].ParseToDecimal()) ?? 0;
 
             var model = new XlsDataRowModel
             {
-                TransactionDate = dataRow[0].ParseToDateTime(),
-                TransactionDescription = dataRow[1]?.ToString() ?? string.Empty,
+                TransactionDate = dataRow[XlsConstants.TransactionDateIndex].ParseToDateTime(),
+                TransactionDescription = dataRow[XlsConstants.TransactionDescriptionIndex]?.ToString() ?? string.Empty,
                 TransactionAmount = transactionOutflow > 0 ? transactionOutflow * -1 : transactionInflow > 0 ? transactionInflow : 0,
-                TransactionAccountName = dataRow[4]?.ToString() ?? string.Empty,
-                TransferDate = dataRow[9].ParseToDateTime(),
-                TransferDescription = dataRow[10]?.ToString() ?? string.Empty,
-                TransferAmount = Utils.DecimalToCubedInt(dataRow[11].ParseToDecimal()) ?? 0
+                TransactionAccountName = dataRow[XlsConstants.TransactionAccountNameIndex]?.ToString() ?? string.Empty,
+                TransferDate = dataRow[XlsConstants.TransferDateIndex].ParseToDateTime(),
+                TransferDescription = dataRow[XlsConstants.TransferDescriptionIndex]?.ToString() ?? string.Empty,
+                TransferAmount = Utils.DecimalToCubedInt(dataRow[XlsConstants.TransferAmountIndex].ParseToDecimal()) ?? 0
             };
 
             return model;
@@ -42,7 +41,7 @@ namespace RDS.ExpenseTracker.Business.TransactionImport.Parsers
 
         public static Transaction ExtractStandardTransaction(XlsDataRowModel model)
         {
-            return new Transaction
+            var transaction = new Transaction
             {
                 Amount = model.TransactionAmount,
                 Date = model.TransactionDate,
@@ -51,33 +50,41 @@ namespace RDS.ExpenseTracker.Business.TransactionImport.Parsers
                 IsTransfer = false,
                 CategoryName = string.Empty
             };
+
+            return transaction;
         }
 
         public static Transaction ExtractOutgoingTransfer(XlsDataRowModel model)
         {
-            return new Transaction
+            var transaction = new Transaction
             {
                 Amount = model.TransferAmount * -1,
                 Date = model.TransferDate,
                 Description = model.TransferDescription,
-                CategoryName = "SpostamentiDenaro",
-                FinancialAccountName = "Sella",
+                FinancialAccountName = XlsConstants.SellaAccountName,
                 IsTransfer = true
             };
+
+            return transaction;
         }
 
         public static Transaction ExtractIngoingTransfer(XlsDataRowModel rowModel)
         {
-            var accountName = rowModel.TransferDescription.ToLower().Contains("hype") ? "Hype" : rowModel.TransferDescription.ToLower().Contains("satispay") ? "Satispay" : string.Empty;
-            return new Transaction
+            var accountName = rowModel.TransferDescription.ToLower().Contains(XlsConstants.HypeAccountName.ToLower())
+                ? XlsConstants.HypeAccountName
+                : rowModel.TransferDescription.ToLower().Contains(XlsConstants.SatispayAccountName.ToLower())
+                ? XlsConstants.SatispayAccountName : string.Empty;
+
+            var transaction = new Transaction
             {
                 Amount = rowModel.TransferAmount,
                 Date = rowModel.TransferDate,
                 Description = rowModel.TransferDescription,
-                CategoryName = "SpostamentiDenaro",
                 FinancialAccountName = accountName,
                 IsTransfer = true
             };
+
+            return transaction;
         }
 
         public static void AssignDateIfMissing(Transaction transaction, DateTime defaultDate)
