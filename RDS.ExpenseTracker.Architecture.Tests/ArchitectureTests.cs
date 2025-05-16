4683a703034e4ecaf983425ba671f6ddfa7bbda1
Roberto De Salvo <roberto.desalvo@outlook.com>
2025-05-16 09:20:29 +0200

    added architecture tests

--- RDS.ExpenseTracker.Architecture.Tests/ArchitectureTests.cs
+++ RDS.ExpenseTracker.Architecture.Tests/ArchitectureTests.cs
@@ -1,4 +1,10 @@
-﻿namespace RDS.ExpenseTracker.Architecture.Tests
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+using RDS.ExpenseTracker.Api.Architecture;
+using RDS.ExpenseTracker.Business.Architecture;
+using RDS.ExpenseTracker.DataAccess.Architecture;
+
+namespace RDS.ExpenseTracker.Architecture.Tests
 {
     public class ArchitectureTests
     {
@@ -7,12 +13,34 @@
         public void Presentation_ShouldNotReference_Data()
         {
             var result = Types
-                .InAssembly(typeof(PresentationNamespaceMarker).Assembly)
+                .InAssembly(typeof(PresentationAssemblyMarker).Assembly)
                 .ShouldNot()
-                .HaveDependencyOn("YourApp.Data")
+                .HaveDependencyOn(typeof(DataAccessAssemblyMarker).Assembly.GetName().Name)
                 .GetResult();
 
-            Assert.True(result.IsSuccessful, "Presentation layer should not depend on Data layer.");
+            result.IsSuccessful.Should().BeTrue("Presentation should not have direct dependencies on Data");
+        }
+
+        [Theory]
+        [InlineData(typeof(PresentationAssemblyMarker))]
+        [InlineData(typeof(DataAccessAssemblyMarker))]
+        [InlineData(typeof(BusinessAssemblyMarker))]
+        public void All_Classes_Should_Use_Correct_Namespace_Prefix(object assemblyMarker)
+        {
+            var assembly = assemblyMarker.GetType().Assembly;
+            var expectedNamespacePrefix = assembly.GetName().Name;
+
+            var failingTypes = Types.InAssembly(assembly)
+                .That()
+                .AreClasses()
+                .GetTypes()
+                .Where(t => t.Namespace != null && !t.Namespace.StartsWith(expectedNamespacePrefix))
+                .ToList();
+
+            failingTypes.Should().BeEmpty(
+                $"All classes in {expectedNamespacePrefix} should have a namespace which starts with '{expectedNamespacePrefix}', but the followings have not:\n" +
+                string.Join("\n", failingTypes.Select(t => t.FullName))
+            );
         }
     }
 }
